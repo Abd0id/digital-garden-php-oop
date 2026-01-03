ec2a5ffd3dfa88da53f6cccb22079867f8e22970
ihmaditaha <ihmaditaha1@gmail.com>
2026-01-03 14:48:09 +0100

    created php classes

---
+++ src/Entity/Note.php
@@ -0,0 +1,32 @@
+<?php
+
+class Note
+{
+
+    private $id;
+    private $title;
+    private $importance;
+    private $content;
+    private $themeId;
+
+
+    public function __construct($title, $importance, $content,$themeId)
+    {
+        $this->title = $title;
+        $this->importance = $importance;
+        $this->content = $content;
+        $this->themeId = $themeId;
+    }
+
+
+    public function getId(){return $this->id;}
+    public function getTitle(){return $this->title;}
+    public function getImportance(){return $this->importance;}
+    public function getContent(){return $this->content;}
+    public function getThemeId(){return $this->themeId;}
+
+    public function setId($id){$this->id = $id;}
+    public function setTitle($title){$this->title = $title;}
+    public function setImportance($importance){$this->importance = $importance;}
+    public function setContent($content){$this->content = $content;}
+}
