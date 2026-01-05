281fbfd183c629fb1195157ca57d423aa78d0ab9
ihmaditaha <ihmaditaha1@gmail.com>
2026-01-05 06:32:04 +0100

    modified entities to match sql

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,6 +25,7 @@ class User {
         $this->role = $role;
     }
 
+    public function getId(){return $this->id;}
     public function getFullName(){return $this->fullName;}
     public function getEmail(){return $this->email;}
     public function getPassword(){return $this->password;}
